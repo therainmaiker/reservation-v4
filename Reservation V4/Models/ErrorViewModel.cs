a81c83fe3e1b32620f5df0d6b7775eb6e42990e7
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2021-03-09 22:53:46 +0100

    package pomeleo added & mysql

---
+++ Reservation V4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reservation_V4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
